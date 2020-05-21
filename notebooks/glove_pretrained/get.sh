95ceace47d76ff2e255d1e20c0f1bcfaac728788
vogier <contact@vogier.fr>
2020-05-21 06:15:29 +0200

    Fix temporary directory creation.

--- notebooks/glove_pretrained/get.sh
+++ notebooks/glove_pretrained/get.sh
@@ -2,7 +2,7 @@
 set -u
 
 WD="$(dirname "$0")"
-FNAME=$(mktemp -p "glove.6B.XXXXXX.zip")
+FNAME=$(mktemp -p "" "glove.6B.XXXXXX.zip")
 
 wget "http://nlp.stanford.edu/data/glove.6B.zip" -O "$FNAME" &&
 unzip -d "$WD" "$FNAME" &&
